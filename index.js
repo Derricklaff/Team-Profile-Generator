a3cdf7d4360e70939228d6cb959f9b57c4ce7771
Derrick Lafforthun <derricklaff@gmail.com>
2022-10-12 22:21:42 -0700

    created pathing to generate new HTML doc

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-// link to page creation
-const generateHTML = require('./src/generateHTML');
-
-// team profiles
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Employee = require('./lib/Employee')
-
-// node modules 
-const fs = require('fs');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 const inquirer = require('inquirer');
-
+const path = require('path');
+const fs = require('fs');
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const generateTeam = require('./src/generateHTML.js')
 
 // Members Array
 const membersArray = [];
@@ -72,10 +66,99 @@ function runApp() {
 
         ]).then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-            teamArray.push(manager);
+            membersArray.push(manager);
             createTeam();
         });
 
     }
 
+    function addEngineer() {
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's name?"
+            },
+
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the engineer's employee ID number?"
+            },
+
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's email address?"
+            },
+
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the engineer's GitHub username?"
+            }
+
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            membersArray.push(engineer);
+            createTeam();
+        });
+
+    }
+    function addIntern() {
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's name?"
+            },
+
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's employee ID number?"
+            },
+
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email address?"
+            },
+
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What school does the intern attend?"
+            }
+
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            membersArray.push(intern);
+            createTeam();
+        });
+
+    }
+
+    // return to menu with option to add another team member create team
+
+    // Would you like to add a team member?
+    // Yes || No
+    // If Yes --> Then select an employee role for your new team member: Manager, Engineer, Intern
+    // If No --> Create Team
+
+
+    function htmlBuilder() {
+        console.log("Team created!")
+
+        fs.writeFileSync(outputPath, generateTeam(membersArray), "UTF-8")
+
+    }
+
+    createTeam()
+
+
 }
+
+runApp();
